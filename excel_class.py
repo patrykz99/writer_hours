5bc885997e3ef9f6f88bb7f05e9ece519e2f2dd0
unknown <patryk.ziolo12@gmail.com>
2022-12-05 00:11:07 +0100

    Added new method (not done yet). Currently, it looks for proper cell

--- excel_class.py
+++ excel_class.py
@@ -2,6 +2,7 @@ import openpyxl
 import xlsxwriter
 import os
 from time import sleep
+import datetime
 
 
 class excel_hours():
@@ -10,20 +11,6 @@ class excel_hours():
         self.months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July',
                        8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
         self.path = Path_to_file
-
-    def create_excel(self):
-        new_excel_file = xlsxwriter.Workbook(
-            self.path+self.fileName)
-
-        for k, v in self.months.items():
-            Work_sheet = new_excel_file.add_worksheet(f'{v[:3]}')
-            self.new_sheet_and_fill(new_excel_file, Work_sheet, k)
-
-        new_excel_file.close()
-        # dodac zeby np. po 5 min usuwalo ten plik to tylko do modyfikowania
-
-        sleep(180)
-        os.remove(self.path+self.fileName)
 
     def new_sheet_and_fill(self, excel_file, worksheet_name, current_month):
         """
@@ -37,31 +24,74 @@ class excel_hours():
         def make_dates_for_respective_months(row_number):
             '''fill dates and mark weekends'''
             if (current_month == 2) and (row_number - 4 <= 28):
-                date = f'{row_number-4}/{current_month}/2022'
-
-                # if first_day_in_the_year.weekday() in (5, 6):
-                #     for col in range(4, 10):
-                #         worksheet_name.write(row_number, col, None, excel_file.add_format(
-                #             {'bg_color': 'black'}))
-                # else:
-                #     for col in range(4, 9, 2):
-                #         worksheet_name.merge_range(
-                #             row_number, col, row_number, (col+1), None, cell_border)
+                date = datetime.date(
+                    year=2022, month=current_month, day=row_number-4)
+                if date.weekday() in (5, 6):
+                    for col in range(4, 10):
+                        worksheet_name.write(row_number, col, None, excel_file.add_format(
+                            {'bg_color': 'red'}))
+                else:
+                    for col in range(4, 9, 2):
+                        if col == 8:
+                            if row_number == 32:
+                                worksheet_name.merge_range(
+                                    row_number, col, row_number, col+1, None, excel_file.add_format({'bottom': 1, 'right': 1}))
+                                break
+                            worksheet_name.merge_range(
+                                row_number, col, row_number, col+1, None, excel_file.add_format({'right': 1}))
+                            break
+                        worksheet_name.merge_range(
+                            row_number, col, row_number, col+1, None, cell_border)
                 '''Write dates'''
 
                 worksheet_name.merge_range(
-                    row_number, 2, row_number, 3, date, cell_border)
-            elif (current_month % 2 == 0 and current_month != 2) and (row_number - 4 <= 30):
-                date = f'{row_number-4}/{current_month}/2022'
+                    row_number, 2, row_number, 3, date.strftime('%d/%m/%Y'), cell_border)
+            elif (current_month % 2 == 0 and current_month != 2 and current_month < 8) and (row_number - 4 <= 30)\
+                    or (current_month % 2 != 0 and current_month != 2 and current_month >= 8) and (row_number - 4 <= 30):
+                date = datetime.date(
+                    year=2022, month=current_month, day=row_number-4)
+                if date.weekday() in (5, 6):
+                    for col in range(4, 10):
+                        worksheet_name.write(row_number, col, None, excel_file.add_format(
+                            {'bg_color': 'red'}))
+                else:
+                    for col in range(4, 9, 2):
+                        if col == 8:
+                            if row_number == 34:
+                                worksheet_name.merge_range(
+                                    row_number, col, row_number, col+1, None, excel_file.add_format({'bottom': 1, 'right': 1}))
+                                break
+                            worksheet_name.merge_range(
+                                row_number, col, row_number, col+1, None, excel_file.add_format({'right': 1}))
+                            break
+                        worksheet_name.merge_range(
+                            row_number, col, row_number, col+1, None, cell_border)
 
                 worksheet_name.merge_range(
-                    row_number, 2, row_number, 3, date, cell_border)
+                    row_number, 2, row_number, 3, date.strftime('%d/%m/%Y'), cell_border)
             else:
-                if current_month % 2 != 0:
-                    date = f'{row_number-4}/{current_month}/2022'
+                if (current_month % 2 != 0 and current_month < 8) or (current_month % 2 == 0 and current_month >= 8):
+                    date = datetime.date(
+                        year=2022, month=current_month, day=row_number-4)
+                    if date.weekday() in (5, 6):
+                        for col in range(4, 10):
+                            worksheet_name.write(row_number, col, None, excel_file.add_format(
+                                {'bg_color': 'red'}))
+                    else:
+                        for col in range(4, 9, 2):
+                            if col == 8:
+                                if row_number == 35:
+                                    worksheet_name.merge_range(
+                                        row_number, col, row_number, col+1, None, excel_file.add_format({'bottom': 1, 'right': 1}))
+                                    break
+                                worksheet_name.merge_range(
+                                    row_number, col, row_number, col+1, None, excel_file.add_format({'right': 1}))
+                                break
+                            worksheet_name.merge_range(
+                                row_number, col, row_number, col+1, None, cell_border)
 
                     worksheet_name.merge_range(
-                        row_number, 2, row_number, 3, date, cell_border)
+                        row_number, 2, row_number, 3, date.strftime('%d/%m/%Y'), cell_border)
 
         def make_border_for_cells(len_month=36):
             for c in range(2, 10):
@@ -78,9 +108,15 @@ class excel_hours():
         if current_month == 2:
             make_border_for_cells(33)
         elif current_month % 2 == 0:
-            make_border_for_cells(35)
+            if current_month > 7:
+                make_border_for_cells()
+            else:
+                make_border_for_cells(35)
         else:
-            make_border_for_cells()
+            if current_month > 7:
+                make_border_for_cells(35)
+            else:
+                make_border_for_cells()
 
         worksheet_name.merge_range(
             2, 2, 2, 9, 'Table with work hours', fst_cell)
@@ -101,3 +137,37 @@ class excel_hours():
                         r, 2, r, 3, self.months[current_month], cell_border)
                 else:
                     make_dates_for_respective_months(r)
+
+    def insert_datas_by_user(self):
+        # respond_date = input(
+        #     'Please enter what date you want fill: Custom date or today\'s date (custom/today) ')
+        respond_hours = input(
+            'Please enter From what hour to what hour u worked, amount of hours you worked (separate with a space) ')
+        # 15-16.30 1.30 15-17 2
+        to_write = respond_hours.split(' ')
+        date_today = datetime.date.today().strftime('%d/%m/%Y')
+        wb = openpyxl.load_workbook(filename=self.path+self.fileName)
+        if self.months[int(date_today[3:5])][0:3] in wb.sheetnames:
+            ws_current = wb[self.months[int(date_today[3:5])][0:3]]
+            for row in ws_current.iter_rows(min_row=6, min_col=3, max_col=4, max_row=36):
+                for cell in row:
+                    if cell.value == date_today:
+                        # skonczylem tutaj tu bedzie trzeba dodac zeby wpisywalo te godziny w cell'u obok
+                        print('jest git')
+
+        # print(date_today.strftime('%d/%m/%Y'))
+        print(to_write, date_today[3:5], type(self.months.keys()))
+
+    def create_excel(self):
+        new_excel_file = xlsxwriter.Workbook(
+            self.path+self.fileName)
+
+        for k, v in self.months.items():
+            Work_sheet = new_excel_file.add_worksheet(f'{v[:3]}')
+            self.new_sheet_and_fill(new_excel_file, Work_sheet, k)
+
+        new_excel_file.close()
+        # dodac zeby np. po 5 min usuwalo ten plik to tylko do modyfikowania
+
+        # sleep(180)
+        # os.remove(self.path+self.fileName)
