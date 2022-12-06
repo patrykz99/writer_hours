4b125c795883368744ddb1f9288ae2ef92bf0ee1
unknown <patryk.ziolo12@gmail.com>
2022-12-07 00:28:26 +0100

    Main function to execute program (create excel file or just update it)

--- main_execute.py
+++ main_execute.py
@@ -2,18 +2,24 @@ from excel_class import excel_hours
 import os.path
 
 
-def check_and_execute(File_name:str,Path_to_file:str):
+def check_and_execute(File_name: str, Path_to_file: str):
+    """
+        Function to check: 
+        if excel exists (create new file and insert data from user) or not (open exisiting and insert data from user)
+    Args:
+        File_name (str): Excel file name
+        Path_to_file (str): Path to excel file
+    """
+    new_excel = excel_hours(File_name, Path_to_file)
     if os.path.isfile(Path_to_file + File_name):
-        print("false")
+        new_excel.insert_hours_by_user_choice()
     else:
-        new_excel = excel_hours(File_name,Path_to_file)
         new_excel.create_excel()
-        
-    
+        new_excel.insert_hours_by_user_choice()
 
 
 if __name__ == "__main__":
-    #invoke
+    # main invoke
     File_name = "Work_hours.xlsx"
-    Path_to_file = "C:/Users/onvtsh/Desktop/" 
-    check_and_execute(File_name,Path_to_file)
+    Path_to_file = "C:/Users/Admin/Desktop/"
+    check_and_execute(File_name, Path_to_file)
